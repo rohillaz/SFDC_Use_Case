e36cc4dcd51779091dc69ef078928a05ad5c6a9d
Afzal Khan <khan.arkafzal@gmail.com>
2021-12-02 17:14:52 -0500

    Create LightningComp.js

---
+++ LightningComp.js
@@ -0,0 +1,49 @@
+import { LightningElement ,api, wire, track} from 'lwc';
+import getAccountList from '@salesforce/apex/AccountHelper.getAccountList';
+export default class LightningDatatableLWCExample extends LightningElement {
+    @track columns = [{
+            label: 'Account name',
+            fieldName: 'Name',
+            type: 'text',
+            sortable: true
+        },
+        {
+            label: 'Account Owner',
+            fieldName: 'Owner',
+            type: 'text',
+            sortable: true
+        },
+        {
+            label: 'Annual Revenue',
+            fieldName: 'AnnualRevenue',
+            type: 'Currency',
+            sortable: true
+        },
+        {
+            label: 'Phone',
+            fieldName: 'Phone',
+            type: 'phone',
+            sortable: true
+        },
+        {
+            label: 'Website',
+            fieldName: 'Website',
+            type: 'url',
+            sortable: true     
+        }
+    ];
+ 
+    @track error;
+    @track accList ;
+    @wire(getAccountList)
+    wiredAccounts({
+        error,
+        data
+    }) {
+        if (data) {
+            this.accList = data;
+        } else if (error) {
+            this.error = error;
+        }
+    }
+}
